091a181ec4e23b3cbbdf1170ad59a8841289b7ab
Jackie Liu <jackolake@gmail.com>
2023-12-12 22:49:04 +0800

    Day 12 - dynamic programming

--- src/day_12.py
+++ src/day_12.py
@@ -1,36 +1,38 @@
 from typing import List, Dict, Tuple
-from itertools import product
-import re
+from functools import cache
 
-def check(onsen_list: str, group_sizes: List[int]) -> bool:
-    return [len(p) for p in re.findall('#+', onsen_list)] == group_sizes
+@cache
+def run(seq: str, cluster: Tuple[int]) -> int:
+    # Termination: Good if no onsen left and no pending cluster
+    if not seq:
+        return 1 if not cluster else 0
+    elif not cluster:
+        return 1 if '#' not in seq else 0
+    value = 0
+    if seq[0] in ('.', '?'):            # 1) .### => ###  2) ?### => .### => ###
+        value += run(seq[1:], cluster)  # treat leftmost as processed and count the rest
+    if seq[0] in ('#', '?'):                                          # 3) #### / ?### => try to match current cluster target
+        target_count = cluster[0]
+        substring = seq[:target_count]
+        if len(substring) == target_count and '.' not in substring:  
+            if len(seq) == target_count or seq[target_count] != '#':  # Also the onsen after this is not broken (i.e. not #####)
+                value += run(seq[target_count + 1:], cluster[1:])     # matched a cluster, move to the next cluster
+    return value
 
-def gen_combinations(input_text: str) -> List[str]:
-    replace_count = input_text.count('?')
-    replacements = list([''.join(p) for p in product(['.', '#'], repeat=replace_count)])
-    replace_indices = [i for i, s in enumerate(input_text) if s == '?']
-    return [''.join([replacement[replace_indices.index(i)] if i in replace_indices else input_text[i]
-                     for i in range(len(input_text))])
-                     for replacement in replacements]
 
-def part1(onsen_group_list: List[str], good_onsen_size_list: List[List[int]]) -> int:
-    return sum([len([g for g in gen_combinations(onsen_group) if check(g, group_sizes)])
-                for onsen_group, group_sizes in zip(onsen_group_list, good_onsen_size_list)])
+def part1(input_list: Tuple[Tuple[str, Tuple[int]]]) -> int:
+    return sum([run(seq, cluster) for seq, cluster in input_list])
 
-def part2(onsen_group_list: List[str], good_onsen_size_list: List[List[int]]) -> int:
-    onsen_group_list = [(l * 5) for l in onsen_group_list]
-    good_onsen_size_list = [(l * 5) for l in good_onsen_size_list]
-    return part1(onsen_group_list, good_onsen_size_list)
-
+def part2(input_list: Tuple[Tuple[str, Tuple[int]]]) -> int:
+    # "replace the list of spring conditions with five copies of itself (separated by ?)" (I missed the "?" initially)
+    return part1([('?'.join([seq] * 5), cluster * 5) for seq, cluster in input_list])
 
 if __name__ == '__main__':
-    onsen_sequence_list = []
-    good_onsen_size_list = []
+    input_list = []  # (sequence, cluster)   e.g. [('?###????????', [3,2,1])]
     with open('input/day_12.txt', 'r') as f:
-        lines = [l.strip() for l in f.readlines() if l.strip()]
-        for line in lines:
-            symbol_list, number_list = line.split(' ') # '#.#.### 1,1,3'
-            onsen_sequence_list.append(symbol_list)
-            good_onsen_size_list.append([int(n) for n in number_list.split(',')])
-    print(f'Part 1: {part1(onsen_sequence_list, good_onsen_size_list)}')
-    print(f'Part 2: {part2(onsen_sequence_list, good_onsen_size_list)}')
+        for line in f.readlines():
+            seq, cluster = line.strip().split(' ')
+            input_list.append((seq, tuple([int(n) for n in cluster.split(',')])))
+            
+    print(f'Part 1: {part1(input_list)}')
+    print(f'Part 2: {part2(input_list)}')
